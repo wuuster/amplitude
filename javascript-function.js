a9965f8a4ce106ad819645ad5fb33900c0d8cd5c
Koiwu Beyan <wuuster@gmail.com>
2015-07-01 12:02:07 -0700

    Update javascript-function.js

--- javascript-function.js
+++ javascript-function.js
@@ -1,19 +1,3 @@
-/*Write a function:
-
-function solution(A);
-
-that, given a non-empty zero-indexed array A consisting of N integers, returns its amplitude.
-
-Assume that:
-
-N is an integer within the range [1..1,000,000];
-each element of array A is an integer within the range [0..5,000,000].
-For example, given array A such that:
-
-  A[0] = 10  A[1] = 2   A[2] = 44
-  A[3] = 15  A[4] = 39  A[5] = 20
-the function should return 42.
-*/
 
 var A = [10, 2, 44, 15, 39, 20];
 
