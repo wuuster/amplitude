ea7217d0370507640ba8fc6bea107df5f4af5390
Koiwu Beyan <wuuster@gmail.com>
2016-03-10 16:16:38 -0800

    Update javascript-function.js

--- javascript-function.js
+++ javascript-function.js
@@ -2,11 +2,7 @@
 var A = [10, 2, 44, 15, 39, 20];
 
 function solution(A) {	
-	var B = Math.max.apply(Math, A);
-	var C = Math.min.apply(Math, A);
-	var D = eval(Math.abs(B-C));
-  console.log(D);
-  return D;
+  A.sort(); return { A[A.length-1] - A[0] };
 }
 
 solution(A);
